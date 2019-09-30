9644e097d9f62f9499e5f09671c2019a8c297951
monkukui <monkukui1953@gmail.com>
2019-09-30 13:32:05 +0900

    reset client

--- client/src/components/Answers.vue
+++ client/src/components/Answers.vue
@@ -1,5 +1,4 @@
 <template>
-  <p> hogehogehogehoge </p>
   <div class="answers">
     <div v-for="(value, index) in answers" :key=index>
       <AnswerPanelDetail
@@ -29,9 +28,9 @@ export default class Answers extends Vue {
 
   // 質問をページ取得する
   private getAnswers(): void {
+
     const url = 'api/answers/' + String(this.questionId);
     const headers = {Authorization: `Bearer ${this.getToken()}`};
-
     fetch(url, {headers}).then((response) => {
       if (response.ok) {
         return response.json();
