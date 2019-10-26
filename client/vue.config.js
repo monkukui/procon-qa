d3cc4a834ab8f20e299b6c4f6463a1cbb1596c7d
sono8 <sono.stream8580@gmail.com>
2019-10-26 18:55:05 +0900

    Add proxy setting

---
+++ client/vue.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  devServer: {
+    proxy: {
+      "/login": {
+        target: "http://localhost:8080",
+        changeOrigin: true
+      },
+      "/signup": {
+        target: "http://localhost:8080",
+        changeOrigin: true
+      },
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true
+      }
+    }
+  }
+};
