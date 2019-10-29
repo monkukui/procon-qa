c98a4cd799757dea7d91996cea9130346292dcaf
monkukui <monkukui1953@gmail.com>
2019-10-30 01:05:33 +0900

    change main.go

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
+import (
+  "os"
+)
+
 func main() {
 	router := newRouter()
-	router.Logger.Fatal(router.Start(":8080"))
+	router.Logger.Fatal(router.Start(":"+os.Getenv("PORT")))
 }
