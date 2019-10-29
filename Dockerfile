f6982c7d31eef4ed44225a45c7e6908a97c396b1
monkukui <monkukui1953@gmail.com>
2019-10-29 23:02:44 +0900

    add doxker

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:lts-alpine
+FROM node:lts-alpine AS build-js
 
 WORKDIR /app
 
@@ -9,3 +9,29 @@ RUN npm install --verbose
 COPY ./client ./
 
 RUN npm run build 
+
+FROM golang:alpine AS build-go
+
+WORKDIR /app
+
+COPY ./go.* ./
+
+COPY ./*.go ./
+
+COPY ./handler ./handler
+
+COPY ./model ./model
+
+RUN CGO_ENABLED=0 go build -o main main.go router.go
+
+FROM busybox
+
+WORKDIR /app
+
+COPY --from=build-js /app/dist ./dist
+
+COPY --from=build-go /app/main ./main
+
+EXPOSE 8080
+
+ENTRYPOINT ["./main"]
--- model/db.go
+++ model/db.go
@@ -1,8 +1,10 @@
 package model
 
 import (
+    "os"
     "github.com/jinzhu/gorm"
     _ "github.com/jinzhu/gorm/dialects/postgres" // Use PostgreSQL in gorm
+    "github.com/lib/pq"
     // _ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
@@ -11,7 +13,28 @@ var db *gorm.DB
 func init() {
 	var err error
   // db, err = gorm.Open("sqlite3", "db/hoge.db")
-  db, err = gorm.Open("postgres", "user=postgres password=postgres dbname=huga sslmode=disable")
+
+  url := os.Getenv("DATABASE_URL")
+  env := ""
+
+  if "" == url {
+    env = "development"
+  } else {
+    env = "production"
+  }
+
+
+  if env == "development" {
+    db, err = gorm.Open("postgres", "user=postgres password=postgres dbname=huga sslmode=disable")
+  } else {
+
+    connection, err := pq.ParseURL(url)
+    if err != nil {
+      panic(err.Error())
+    }
+    connection += " sslmode=require"
+    db, err = gorm.Open("postgres", connection);
+  }
 	if err != nil {
 		panic(err)
 	}
