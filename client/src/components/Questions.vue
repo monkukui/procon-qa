9f4ec0831c367bee6be468fbf81199f66cd871ec
monkukui <monkukui1953@gmail.com>
2019-11-17 23:18:06 +0900

    ページネーション，正しく切り上げ

--- client/src/components/Questions.vue
+++ client/src/components/Questions.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="questions">
+    {{ length }}
     <v-pagination
       v-model="curPageId"
       :length="length"
@@ -74,7 +75,7 @@ export default class Questions extends Vue {
       return [];
     }).then((cnt) => {
       this.totalQuestions = cnt;
-      this.length = (this.totalQuestions + 9) / 10; // 切り上げ
+      this.length = Math.ceil(this.totalQuestions / 10); // 切り上げ
     });
   }
 
