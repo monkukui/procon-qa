8810e348b9739169a44b5c12908e76aa86a057e3
monkukui <monkukui1953@gmail.com>
2020-04-25 19:33:11 +0900

    FIX: 設定の UI を少し変更

--- client/src/components/MySetting.vue
+++ client/src/components/MySetting.vue
@@ -1,7 +1,39 @@
 <template>
   <div class="myprofile">
-    <UserUpdateForms />
-    <WithdrawButton />
+    <v-card>
+      <v-tabs vertical height="400" hide-slider>
+        <v-tab>
+          <v-icon left>mdi-account</v-icon>
+          基本設定
+        </v-tab>
+        <v-tab>
+          <v-icon left>mdi-delete</v-icon>
+          アカウント削除
+        </v-tab>
+        <!-- v-tab>
+          <v-icon left>mdi-access-point</v-icon>
+          Option 3
+        </v-tab-->
+
+        <v-tab-item>
+          <v-card flat>
+            <UserUpdateForms />
+          </v-card>
+        </v-tab-item>
+        <v-tab-item>
+          <v-card flat>
+            <WithdrawButton />
+          </v-card>
+        </v-tab-item>
+        <v-tab-item>
+          <v-card flat>
+            <v-card-text>
+            </v-card-text>
+          </v-card>
+        </v-tab-item>
+      </v-tabs>
+    </v-card>
+
   </div>
 </template>
 
