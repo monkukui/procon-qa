235b926446201b4c2400f0d2ce2ea36800151523
monkukui <monkukui1953@gmail.com>
2020-05-09 14:32:33 +0900

    Add: レーティング機能の追加

--- client/src/components/UserName.vue
+++ client/src/components/UserName.vue
@@ -1,11 +1,11 @@
 <template>
-  <span v-bind:class="color">
+  <span>
     <span v-if="isExist">
       <router-link 
-        class="name"
+        :class="color"
         :to="{ name: 'userpage', query: { uid: this.uid }}"
       >
-        {{ name }}
+        <span class="name">{{ name }}</span>
       </router-link>
     </span>
     <span v-else>
@@ -22,12 +22,11 @@ import { Vue, Component, Prop } from 'vue-property-decorator';
 export default class UserName extends Vue {
   @Prop()
   private uid!: string;
-  @Prop()
-  private color!: string;
+
+  private color: string = '';
 
   private name: string = '';
   private isExist: boolean = false;
-
 
   private created(): void {
     this.setUser();
@@ -42,10 +41,33 @@ export default class UserName extends Vue {
       return [];
     }).then((json) => {
       this.name = json.name;
+      let favoriteSum = json.favorite_sum;
+      console.log(json);
       if (this.name === '') {
         this.name = '退会済みのユーザ';
       } else {
         this.isExist = true;
+      }
+
+
+      if (favoriteSum <= 0) {
+        this.color = 'user-unrated';
+      } else if (favoriteSum <= 1) {
+        this.color = 'user-gray';
+      } else if (favoriteSum <= 2) {
+        this.color = 'user-brown';
+      } else if (favoriteSum <= 4) {
+        this.color = 'user-green';
+      } else if (favoriteSum <= 8) {
+        this.color = 'user-cyan';
+      } else if (favoriteSum <= 16) {
+        this.color = 'user-blue';
+      } else if (favoriteSum <= 32) {
+        this.color = 'user-yellow';
+      } else if (favoriteSum <= 64) {
+        this.color = 'user-orange';
+      } else {
+        this.color = 'user-red';
       }
     });
   }
@@ -53,23 +75,51 @@ export default class UserName extends Vue {
 </script>
 
 <style scoped>
-.user-red {color:#FF0000;}
-.user-orange {color:#FF8000;}
-.user-yellow {color:#C0C000;}
-.user-blue {color:#0000FF;}
-.user-cyan {color:#00C0C0;}
-.user-green {color:#008000;}
-.user-brown {color:#804000;}
-.user-gray {color:#808080;}
-.user-unrated {color:#000000;}
-.user-admin {color:#C000C0;}
-.username>span {font-weight:bold;}
+.user-red {
+  color:#FF0000;
+  text-decoration: none;
+}
+.user-orange {
+  color:#FF8000;
+  text-decoration: none;
+}
+.user-yellow {
+  color:#C0C000;
+  text-decoration: none;
+}
+.user-blue {
+  color:#0000FF;
+  text-decoration: none;
+} 
+.user-cyan {
+  color:#00C0C0;
+  text-decoration: none;
+}
+.user-green {
+  color:#008000;
+  text-decoration: none;
+}
+.user-brown {
+  color:#804000;
+  text-decoration: none;
+}
+.user-gray {
+  color:#808080;
+  text-decoration: none;
+}
+.user-unrated {
+  color:#000000;
+  text-decoration: none;
+}
+.user-admin {
+  color:#C000C0;
+  text-decoration: none;
+}
 .name {
   font-weight: bold;
   overflow: hidden;
   text-overflow: ellipsis;
   white-space: nowrap;
-  color: rgb(66, 66, 66);
   text-decoration: none;
 }
 </style>
