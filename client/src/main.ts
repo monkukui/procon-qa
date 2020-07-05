d21e4cd8b4faae71a4dc0d1659acd9f694de027f
sono8 <sono.stream8580@gmail.com>
2020-07-05 15:55:40 +0900

    Add analytics setting

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,12 +1,17 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import vuetify from './plugins/vuetify';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import vuetify from "./plugins/vuetify";
+import VueGtag from "vue-gtag";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueGtag, {
+  config: { id: process.env.VUE_APP_GA_ID },
+});
 
 new Vue({
   router,
   vuetify,
   render: (h) => h(App),
-}).$mount('#app');
+}).$mount("#app");
