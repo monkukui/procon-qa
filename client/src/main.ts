99aa4e093f392902903069ab629dae66799e9d54
sono8 <sono.stream8580@gmail.com>
2020-07-05 16:22:55 +0900

    Fix conflict

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,10 +1,13 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import vuetify from './plugins/vuetify';
-import VueGtag from 'vue-gtag';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import vuetify from "./plugins/vuetify";
+import mavonEditor from "mavon-editor";
+import "mavon-editor/dist/css/index.css";
+import VueGtag from "vue-gtag";
 
 Vue.config.productionTip = false;
+Vue.use(mavonEditor);
 
 Vue.use(VueGtag, {
   config: { id: process.env.VUE_APP_GA_ID },
@@ -14,4 +17,4 @@ new Vue({
   router,
   vuetify,
   render: (h) => h(App),
-}).$mount('#app');
+}).$mount("#app");
