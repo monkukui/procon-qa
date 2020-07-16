cdba9ee31d25eee6e4a32ca6f121c619ea8d957d
monkukui <monkukui1953@gmail.com>
2020-07-16 17:29:34 +0900

    Fix: icon を非表示に

--- client/src/components/TwitterIcon.vue
+++ client/src/components/TwitterIcon.vue
@@ -3,19 +3,19 @@
     <!--v-avatar color="indigo">
       <v-icon dark>mdi-account-circle</v-icon>
     </v-avatar-->
-    <span v-if="twitterId">
+    <!-- span v-if="twitterId">
       <router-link 
         class="name"
         :to="{ name: 'userpage', query: { uid: this.uid }}"
       >
-        <!--v-avatar :size=size>
+        <v-avatar :size=size>
           <img
             :src=imgUrl
             alt="icon"
           >
-        </v-avatar-->
+        </v-avatar>
       </router-link>
-    </span>
+    </span-->
   </div>
 </template>
 
